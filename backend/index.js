33d9c1b314db42065af98f67fd2cb4b783556d64
Rafi Ardiansyah <rafia@unitedtractors.com>
2025-04-15 23:04:08 +0700

    ✨ : integrate microservices with proxy

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const port = 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/hello", (req, res) => {
+app.get("/hello", (req, res) => {
   res.json({ message: "Hello from backend!" });
 });
 
